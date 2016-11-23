6fe76246f041ecb8d95f3393d3a06b1791372671
Kamran Baig <mindbender39@gmail.com>
2016-11-23 11:47:47 +0500

    reset contacts screen both in admin and practitioner

--- resources/views/admin/contact/edit.blade.php
+++ resources/views/admin/contact/edit.blade.php
@@ -29,7 +29,7 @@
     </div>
     {!! Form::model($contact, array('url'=>'/admin/contact/update', 'method' => 'PATCH', 'id'=>'frm-contact', 'class'=> 'form-horizontal', 'files'=>true,'data-parsley-validate' => 'true')) !!}
     <!-- begin col-8 -->
-    <div class="col-md-8">
+    <div class="col-md-7">
         <!-- begin panel -->
         <div class="panel panel-inverse" data-sortable-id="form-stuff-3">
             <div class="panel-heading">
@@ -150,7 +150,7 @@
     </div>
     <!-- end col 8 -->
     <!-- begin col-4 -->
-    <div class="col-md-4">
+    <div class="col-md-5">
         <div class="panel panel-inverse" data-sortable-id="form-stuff-3">
             <div class="panel-heading">
                 <div class="panel-heading-btn">
