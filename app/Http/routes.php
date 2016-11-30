6ff8429ef680ee5319452a3fd78f28e619630a51
Kamran Baig <mindbender39@gmail.com>
2016-11-30 17:22:42 +0500

    Plivo sms api integrated

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
+use Plivo\RestAPI;
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -420,6 +421,36 @@ Route::get('/info/{slug}', [
 
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('users','UserController');
+});
+
+Route::get('test-pilvo', function(){
+    $plivo_credentials = config('plivo.credentials');
+    $api = new RestAPI($plivo_credentials['auth_id'], $plivo_credentials['auth_token']);
+
+    // Set message parameters
+    $params = array(
+        'src' => '1111111111', // Sender's phone number with country code
+        'dst' => '2222222222', // Receiver's phone number with country code
+        'text' => 'Hi, This is a test message from Plivo', // Your SMS text message
+        // To send Unicode text
+        //'text' => 'こんにちは、元気ですか？' # Your SMS Text Message - Japanese
+        //'text' => 'Ce est texte généré aléatoirement' # Your SMS Text Message - French
+        //'url' => 'http://example.com/report/', // The URL to which with the status of the message is sent
+        'method' => 'POST' // The method used to call the url
+    );
+
+    // Send message
+    $response = $api->send_message($params);
+    
+    // Print the response
+    echo "Response : ";
+    print_r ($response['response']);
+
+    // Print the Api ID
+    echo "<br> Api ID : {$response['response']['api_id']} <br>";
+
+    // Print the Message UUID
+    echo "Message UUID : {$response['response']['message_uuid'][0]} <br>";
 });
 
 Route::get('users/admin/login', ['as' => 'login', 'uses' => 'UserController@showAdminLogin']);
